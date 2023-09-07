a4c188c358e0d74f6b9585e4e7756c99fdc583b5
WanchalermMitcachon <127719383+Wanchalerm-Mit@users.noreply.github.com>
2023-09-07 11:01:08 +0700

    xx
    xxx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import Headline from "../shared/Headline";
 import projects from "../../projects";
 
-import { motion } from "framer-motion"
-import {fadeIn} from "../variants"
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+import dopefolio from '../assets/dopefolio.png'
+import wilsonport from '../assets/wilsonport.png'
+import Resume  from '../assets/Resume.png'
 
-import picture from '../assets/Resume.png'
 
 const Projects = () => {
   return (
@@ -17,24 +19,52 @@ const Projects = () => {
           " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porrratione reprehenderit rerum iste excepturi dolorem sequi eos  beatae tempore a."
         }
       />
-      <div>
-        {projects.map((project) => (
-          <motion.div key={project.id} className="projects items.center my-20"
-          variants={fadeIn("up", 0.1)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{once:false, amount:0.2}}
-          >
-            <img src={project.image} alt="xxx" className="max-h-[450px]" />
-            <div className="md:w-10/12 text-center md:text-left px-2">
-              <h4 className="text-2xl font-bold mb-6">{project.name}</h4>
-              <p className="text-lg text-[#666] leading-6">{project.description}</p>
-              <button className="btn py-3 px-7">Case study</button>
-            </div>
-          </motion.div>
-        ))}
-      </div>
-      <img src={picture} alt="" />
+      <motion.div
+        variants={fadeIn("up", 0.1)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.2 }}
+      >
+        <div className="md:grid grid-cols-2 mb-20">
+          <img src={dopefolio} alt="xxx" className="max-h-[450px]" />
+          <div className="md:w-10/12 text-center md:text-left px-2">
+            <h4 className="text-2xl font-bold mb-6">Dopefolio</h4>
+            <p className="text-lg text-[#666] leading-6">
+              Dopefolio is a successful Open-Source project that I created which
+              have been featured on some of the biggest tech sites like
+              CSS-Tricks, Hostinger, etc & used by thousands of developers
+              globally
+            </p>
+            <button className="btn py-3 px-7">Case study</button>
+          </div>
+        </div>
+        <div className="md:grid grid-cols-2 mb-20">
+          <img src={wilsonport} alt="xxx" className="max-h-[450px]" />
+          <div className="md:w-10/12 text-center md:text-left px-2">
+            <h4 className="text-2xl font-bold mb-6">Dopefolio</h4>
+            <p className="text-lg text-[#666] leading-6">
+              Dopefolio is a successful Open-Source project that I created which
+              have been featured on some of the biggest tech sites like
+              CSS-Tricks, Hostinger, etc & used by thousands of developers
+              globally
+            </p>
+            <button className="btn py-3 px-7">Case study</button>
+          </div>
+        </div>
+        <div className="md:grid grid-cols-2 mb-20">
+          <img src={Resume} alt="xxx" className="max-h-[450px] w-full" />
+          <div className="md:w-10/12 text-center md:text-left px-2">
+            <h4 className="text-2xl font-bold mb-6">Dopefolio</h4>
+            <p className="text-lg text-[#666] leading-6">
+              Dopefolio is a successful Open-Source project that I created which
+              have been featured on some of the biggest tech sites like
+              CSS-Tricks, Hostinger, etc & used by thousands of developers
+              globally
+            </p>
+            <button className="btn py-3 px-7">Case study</button>
+          </div>
+        </div>
+      </motion.div>
     </div>
   );
 };
